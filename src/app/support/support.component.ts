0d1f6d9743768d84202274863f835e6e6faebcdd
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2018-10-20 16:14:46 +0100

    complete "Legal", "Status" placeholder pages

--- src/app/support/support.component.ts
+++ src/app/support/support.component.ts
@@ -3,7 +3,8 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
-  templateUrl: './support.component.html'
+  templateUrl: './support.component.html',
+  styleUrls: [ './support.component.css' ]
 })
 export class SupportComponent implements OnInit {
 
