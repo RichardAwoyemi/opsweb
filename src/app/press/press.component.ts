c44ec3ade483a56704430fe4953b4255da400cec
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2018-10-22 00:24:28 +0100

    add press page

--- src/app/press/press.component.ts
+++ src/app/press/press.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './press.component.html'
 })
 export class PressComponent implements OnInit {
+  isMobile: Observable<BreakpointState>;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
+    this.isMobile = this.breakpointObserver.observe(Breakpoints.Handset);
   }
 }
