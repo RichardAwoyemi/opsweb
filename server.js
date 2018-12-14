2fee9ab46b982310187533d96594d9486a955efa
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2018-12-14 23:26:23 +0000

    heroku fix

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var server = require('http').Server(app);
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use( express.static(__dirname + '/dist/opsonion-web' ) );
+app.use(express.static(__dirname + '/dist/opsonion-web'));
 
-var listener = server.listen(process.env.PORT || 5000, function(){
-    console.log('Listening on port ' + listener.address().port);
+app.get('*', function(req, res){
+  res.send('what???', 404);
 });
+
+var listener = server.listen(process.env.PORT || 5000, function () {
+  console.log('Listening on port ' + listener.address().port);
+});
+
