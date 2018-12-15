4bfc0322484a014da306ed8dfa219a2013983f16
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2018-12-15 00:09:36 +0000

    heroku fix

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/dist/opsonion-web'));
 
-app.get('*', function(req, res){
-  res.send('what???', 404);
+app.router.get('*', function (_req, res, _next) {
+  res.sendFile(path.resolve('/dist/opsonion-web/index.html;='));
 });
 
 var listener = server.listen(process.env.PORT || 5000, function () {
