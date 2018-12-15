f69f6caf0dd66289a3b39066a46ea2d9438a1455
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2018-12-15 07:31:30 +0000

    heroku fix

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const http = require('http')
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/opsonion-web/')));
+app.use(express.static('./dist/opsonion-web'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/opsonion-web/index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/opsonion-web/index.html'));
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/opsonion-web/index.html'));
-});
-
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('Running...'));
+app.listen(process.env.PORT || 8080);
