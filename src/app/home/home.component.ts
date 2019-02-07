5990f669808157bb96c4401527135cea5f8aa3b5
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-02-07 21:27:25 +0000

    resize images to improve clarity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { ModalComponent } from '../_modals/modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
 
 declare var $;
 
@@ -14,6 +15,7 @@ declare var $;
 export class HomeComponent implements OnInit {
   isMobile: Observable<BreakpointState>;
   registerFormGroup: FormGroup;
+  submitted = false;
   closeResult: string;
 
   @ViewChild('errorModal') errorModal: ElementRef;
@@ -47,5 +49,30 @@ export class HomeComponent implements OnInit {
     }
 
     const formObject = document.forms['registerForm'];
+
+    if (environment.production === false) {
+      console.log(new FormData(formObject));
+    }
+
+    fetch(this.scriptURL, { method: 'POST', body: new FormData(formObject) })
+      .then(response => {
+        if (environment.production === false) {
+          console.log('Success!', response);
+        }
+        const modalReference = this.modalService.open(ModalComponent, { windowClass: 'modal-holder', centered: true });
+        modalReference.componentInstance.header = 'Yay!';
+        modalReference.componentInstance.message = 'Thanks for signing up. We will be in touch.';
+        this.submitted = true;
+      })
+      .catch(
+        error => {
+          if (environment.production === false) {
+            console.error('Error!', error.message);
+          }
+          const modalReference = this.modalService.open(ModalComponent, { windowClass: 'modal-holder', centered: true });
+          modalReference.componentInstance.header = 'Oops!';
+          modalReference.componentInstance.message = 'Something has gone wrong. Please try again.';
+          this.submitted = true;
+        });
   }
 }
