0a4d613152967557d67d49f3738866a0ee18da30
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-03-11 10:31:37 +0000

    remove logging from tests, fix to ci flow

--- src/app/_services/user.service.spec.ts
+++ src/app/_services/user.service.spec.ts
@@ -30,7 +30,6 @@ describe('UserService unit tests', () => {
       inject([UserService], (userService) => {
         userService.getNumberOfUsers().subscribe((result) => {
           const noOfUsers = result.data.counter;
-          console.log(`Total number of users: ${noOfUsers}`);
           expect(noOfUsers).toBeGreaterThan(0);
           done();
         });
--- src/app/_services/util.service.spec.ts
+++ src/app/_services/util.service.spec.ts
@@ -15,14 +15,10 @@ describe('UtilService unit tests', () => {
       inject([UtilService], (utilService) => {
         let name = 'priscilla';
         let formattedName = utilService.toTitleCase(name);
-        console.log(`Before: ${name}`);
-        console.log(`After: ${formattedName}`);
         expect(formattedName).toEqual('Priscilla');
 
         name = 'JOHN';
         formattedName = utilService.toTitleCase(name);
-        console.log(`Before: ${name}`);
-        console.log(`After: ${formattedName}`);
         expect(formattedName).toEqual('John');
       })
     );
@@ -32,7 +28,6 @@ describe('UtilService unit tests', () => {
     it('should return a random string',
       inject([UtilService], (utilService) => {
         const random = utilService.generateRandomString(8);
-        console.log(`Random string: ${random}`);
         expect(random.length).toEqual(8);
       })
     );
@@ -42,7 +37,6 @@ describe('UtilService unit tests', () => {
     it('should return a range of years',
       inject([UtilService], (utilService) => {
         const years = utilService.createYearRange('2000', '2019');
-        console.log(`Number of years in range: ${years.length}`);
         expect(years.length).toEqual(20);
         expect(years[0]).toEqual(2000);
         expect(years[10]).toEqual(2010);
@@ -58,8 +52,6 @@ describe('UtilService unit tests', () => {
         'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25');
         const androidURL = utilService.getAppStoreLink('Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T)' +
         'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.83 Mobile Safari/537.36');
-        console.log(`iOS mobile app URL: ${iosURL}`);
-        console.log(`Android mobile app URL: ${androidURL}`);
         expect(iosURL).toEqual(utilService.IOS_APP_URL);
         expect(androidURL).toEqual(utilService.ANDROID_APP_URL);
       })
