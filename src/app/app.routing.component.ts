55201acfd88eae73874ed1a87ef8d7a17fb05a4b
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-03-11 11:34:02 +0000

    remove unused routes

--- src/app/app.routing.component.ts
+++ src/app/app.routing.component.ts
@@ -32,65 +32,42 @@ import { TasksComponent } from './tasks/tasks.component';
 import { InviteComponent } from './invite/invite.component';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-import { environment } from 'src/environments/environment';
 
-let routes: Routes = [ ];
-
-if (!environment.referralMode) {
-  routes = [
-    { path: '', component: HomeComponent, canActivate: [AnonymousGuard]  },
-    { path: 'home', component: HomeComponent, canActivate: [AnonymousGuard] },
-    { path: 'about', component: AboutComponent, canActivate: [AnonymousGuard] },
-    { path: 'accounts', component: AccountsComponent, canActivate: [AuthGuard] },
-    { path: 'exchange', component: ExchangeComponent, canActivate: [AuthGuard] },
-    { path: 'careers', component: CareersComponent, canActivate: [AnonymousGuard] },
-    { path: 'contact', component: ContactComponent, canActivate: [AnonymousGuard] },
-    { path: 'legal', component: LegalComponent, canActivate: [AnonymousGuard] },
-    { path: 'login', component: LoginComponent, canActivate: [AnonymousGuard] },
-    { path: 'press', component: PressComponent, canActivate: [AnonymousGuard] },
-    { path: 'markets', component: MarketsComponent, canActivate: [AnonymousGuard] },
-    { path: 'markets/:symbol', component: PriceComponent, canActivate: [AnonymousGuard] },
-    { path: 'register', component: RegisterComponent, canActivate: [AnonymousGuard] },
-    { path: 'status', component: StatusComponent, canActivate: [AnonymousGuard] },
-    { path: 'services', component: ServicesComponent, canActivate: [AnonymousGuard] },
-    { path: 'support', component: SupportComponent, canActivate: [AnonymousGuard] },
-    { path: 'support/account-management', component: AccountManagementComponent, canActivate: [AnonymousGuard] },
-    { path: 'support/transactions', component: TransactionsComponent, canActivate: [AnonymousGuard] },
-    { path: 'support/payment-methods', component: PaymentMethodsComponent, canActivate: [AnonymousGuard]},
-    { path: 'support/security', component: SecurityComponent, canActivate: [AnonymousGuard] },
-    { path: 'support/task-administration', component: TaskAdministrationComponent, canActivate: [AnonymousGuard] },
-    { path: 'support/wallet-services', component: WalletServicesComponent, canActivate: [AnonymousGuard] },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'verify-email', component: VerifyEmailComponent, canActivate: [AnonymousGuard] },
-    { path: 'tools', component: ToolsComponent, canActivate: [AuthGuard] },
-    { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [AnonymousGuard] },
-    { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
-    { path: 'tasks', component: TasksComponent, canActivate: [AuthGuard] },
-    { path: 'task/:id', component: TaskComponent, canActivate: [AuthGuard] },
-    { path: 'tasks/new', component: NewTaskComponent, canActivate: [AuthGuard] },
-    { path: 'invite', component: InviteComponent, canActivate: [AuthGuard] },
-    { path: 'invite/:id', component: InviteComponent, canActivate: [AnonymousGuard] },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
-  ];
-}
-
-if (environment.referralMode) {
-  routes = [
-    { path: '', component: HomeComponent, canActivate: [AnonymousGuard]  },
-    { path: 'home', component: HomeComponent, canActivate: [AnonymousGuard] },
-    { path: 'contact', component: ContactComponent, canActivate: [AnonymousGuard] },
-    { path: 'legal', component: LegalComponent, canActivate: [AnonymousGuard] },
-    { path: 'login', component: LoginComponent, canActivate: [AnonymousGuard] },
-    { path: 'press', component: PressComponent, canActivate: [AnonymousGuard] },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'register', component: RegisterComponent, canActivate: [AnonymousGuard] },
-    { path: 'verify-email', component: VerifyEmailComponent, canActivate: [AnonymousGuard] },
-    { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [AnonymousGuard] },
-    { path: 'invite', component: InviteComponent, canActivate: [AuthGuard] },
-    { path: 'invite/:id', component: InviteComponent, canActivate: [AnonymousGuard] },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
-  ];
-}
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AnonymousGuard]  },
+  { path: 'home', component: HomeComponent, canActivate: [AnonymousGuard] },
+  { path: 'about', component: AboutComponent, canActivate: [AnonymousGuard] },
+  // { path: 'accounts', component: AccountsComponent, canActivate: [AuthGuard] },
+  // { path: 'exchange', component: ExchangeComponent, canActivate: [AuthGuard] },
+  // { path: 'careers', component: CareersComponent, canActivate: [AnonymousGuard] },
+  // { path: 'contact', component: ContactComponent, canActivate: [AnonymousGuard] },
+  { path: 'legal', component: LegalComponent, canActivate: [AnonymousGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AnonymousGuard] },
+  { path: 'press', component: PressComponent, canActivate: [AnonymousGuard] },
+  // { path: 'markets', component: MarketsComponent, canActivate: [AnonymousGuard] },
+  // { path: 'markets/:symbol', component: PriceComponent, canActivate: [AnonymousGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AnonymousGuard] },
+  // { path: 'status', component: StatusComponent, canActivate: [AnonymousGuard] },
+  // { path: 'services', component: ServicesComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support', component: SupportComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support/account-management', component: AccountManagementComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support/transactions', component: TransactionsComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support/payment-methods', component: PaymentMethodsComponent, canActivate: [AnonymousGuard]},
+  // { path: 'support/security', component: SecurityComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support/task-administration', component: TaskAdministrationComponent, canActivate: [AnonymousGuard] },
+  // { path: 'support/wallet-services', component: WalletServicesComponent, canActivate: [AnonymousGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'verify-email', component: VerifyEmailComponent, canActivate: [AnonymousGuard] },
+  // { path: 'tools', component: ToolsComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [AnonymousGuard] },
+  // { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+  // { path: 'tasks', component: TasksComponent, canActivate: [AuthGuard] },
+  // { path: 'task/:id', component: TaskComponent, canActivate: [AuthGuard] },
+  // { path: 'tasks/new', component: NewTaskComponent, canActivate: [AuthGuard] },
+  { path: 'invite', component: InviteComponent, canActivate: [AuthGuard] },
+  { path: 'invite/:id', component: InviteComponent, canActivate: [AnonymousGuard] },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
