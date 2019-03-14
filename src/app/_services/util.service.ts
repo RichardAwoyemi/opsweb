ebbcf95ef12aa62f4da70c87ab94b63fd651762b
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-03-14 15:16:10 +0000

    formatting fix

--- src/app/_services/util.service.ts
+++ src/app/_services/util.service.ts
@@ -7,7 +7,7 @@ export class UtilService {
     return str.replace(
       /\w\S*/g,
       function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       }
     );
   }
