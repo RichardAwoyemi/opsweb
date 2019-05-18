2a7baa3378f6da00b3a12456c957751a7d0ebca6
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-05-18 19:01:31 +0100

    populating libraries column

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -111,6 +111,175 @@ export class NewTaskComponent implements OnInit {
     }
   ];
 
+  authLibraries = [
+    {
+      id: 'email-login',
+      name: 'Email Login'
+    },
+    {
+      id: 'facebook-login',
+      name: 'Facebook Login'
+    },
+    {
+      id: 'google-login',
+      name: 'Google Login'
+    },
+    {
+      id: 'guest-login',
+      name: 'Guest Login'
+    },
+    {
+      id: 'twitter-login',
+      name: 'Twitter Login'
+    },
+    {
+      id: 'forgot-password',
+      name: 'Forgot Password'
+    }
+  ];
+
+  ecommerceLibraries = [
+    {
+      id: 'affiliate-url',
+      name: 'Affiliate URL'
+    },
+    {
+      id: 'bidding',
+      name: 'Bidding'
+    },
+    {
+      id: 'coupons',
+      name: 'Coupons'
+    },
+    {
+      id: 'discounts',
+      name: 'Discounts'
+    },
+    {
+      id: 'shopping-cart',
+      name: 'Shopping Cart'
+    },
+    {
+      id: 'wishlist',
+      name: 'Wishlist'
+    }
+  ];
+
+  financeLibraries = [
+    {
+      id: 'invoice-billing',
+      name: 'Invoice Billing'
+    },
+    {
+      id: 'payments',
+      name: 'Payments'
+    },
+    {
+      id: 'multiple-currency-support',
+      name: 'Multiple Currency Support'
+    },
+    {
+      id: 'split-payments',
+      name: 'Split Payments'
+    },
+    {
+      id: 'subscription-billing',
+      name: 'Subscription Billing'
+    }
+  ];
+
+  gameLibraries = [
+    {
+      id: 'leaderboard',
+      name: 'Leaderboard'
+    }
+  ];
+
+  alertLibraries = [
+    {
+      id: 'desktop-notifications',
+      name: 'Desktop Notifications'
+    },
+    {
+      id: 'email-notifications',
+      name: 'Email Notifications'
+    },
+    {
+      id: 'notification-console',
+      name: 'Notification Console'
+    },
+    {
+      id: 'notification-settings',
+      name: 'Notification Settings'
+    }
+  ];
+
+  multimediaLibraries = [
+    {
+      id: 'archive',
+      name: 'Archive'
+    },
+    {
+      id: 'bookmark',
+      name: 'Bookmark'
+    },
+    {
+      id: 'gallery',
+      name: 'Gallery'
+    },
+    {
+      id: 'groups',
+      name: 'Groups'
+    },
+    {
+      id: 'video-chat',
+      name: 'Video Chat'
+    }
+  ];
+
+  socialLibraries = [
+    {
+      id: 'activity-feed',
+      name: 'Activity Feed'
+    },
+    {
+      id: 'add-friends',
+      name: 'Add Friends/Followers'
+    },
+    {
+      id: 'chat',
+      name: 'Chat'
+    },
+    {
+      id: 'chat-backup',
+      name: 'Chat Backup/Restore'
+    },
+    {
+      id: 'comments',
+      name: 'Comments'
+    },
+    {
+      id: 'emojis',
+      name: 'Emojis'
+    },
+    {
+      id: 'event-registration',
+      name: 'Event Registration'
+    },
+    {
+      id: 'friend-list',
+      name: 'Friend/Follower List'
+    },
+    {
+      id: 'group-chat',
+      name: 'Group Chat'
+    },
+    {
+      id: 'hashtags',
+      name: 'Hashtags'
+    }
+  ];
+
   isMobile: Observable<BreakpointState>;
   task: any;
   innerHeight: number;
@@ -118,6 +287,7 @@ export class NewTaskComponent implements OnInit {
   innerColumnSidebarHeight: number;
 
   @ViewChild('resetModal') resetModal: ElementRef;
+  @ViewChild('requestFeatureModal') requestFeatureModal: ElementRef;
 
   ngOnInit() {
     this.isMobile = this.breakpointObserver.observe([Breakpoints.Handset]);
@@ -128,6 +298,13 @@ export class NewTaskComponent implements OnInit {
     this.logger.debug('Current task is:');
     this.logger.debug(this.task);
     this.logger.debug(`Window height is: ${this.innerHeight}`);
+
+    // temp for testing
+    const config: ScrollToConfigOptions = {
+      target: 'step2'
+    };
+    this.scrollToService.scrollTo(config);
+    document.body.style.overflow = 'hidden';
   }
 
   @HostListener('window:resize', ['$event'])
@@ -145,14 +322,13 @@ export class NewTaskComponent implements OnInit {
       target: 'step2'
     };
     this.scrollToService.scrollTo(config);
+    document.body.style.overflow = 'hidden';
     this.ngxLoader.stop();
   }
 
   selectCategory(categoryId) {
     this.logger.debug(`Category selected: ${categoryId}`);
-    this.ngxLoader.start();
     this.categorySelected = categoryId;
-    this.ngxLoader.stop();
   }
 
   resume() {
@@ -161,6 +337,7 @@ export class NewTaskComponent implements OnInit {
       target: 'step2'
     };
     this.scrollToService.scrollTo(config);
+    document.body.style.overflow = 'hidden';
     this.ngxLoader.stop();
   }
 
@@ -168,10 +345,16 @@ export class NewTaskComponent implements OnInit {
     $(this.resetModal.nativeElement).modal('hide');
     this.productSelected = null;
     this.categorySelected = null;
+    document.body.style.overflow = '';
+    window.scroll(0, 0);
   }
 
   showResetModal() {
     $(this.resetModal.nativeElement).modal('show', { size: 'sm' });
+  }
+
+  showRequestFeatureModal() {
+    $(this.requestFeatureModal.nativeElement).modal('show', { size: 'lg' });
   }
 
   public onIndexChange(index: number): void {
