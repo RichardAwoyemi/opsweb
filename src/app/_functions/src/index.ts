f151b501190e663481b06f6b5c41b3aa2c762c63
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-05-31 22:04:08 +0100

    add support for cors

--- src/app/_functions/src/index.ts
+++ src/app/_functions/src/index.ts
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const cors = require('cors')({origin: true});
 admin.initializeApp(functions.config().firebase);
 
 exports.createUser = functions.firestore
@@ -107,6 +106,7 @@ exports.getAllUsers = functions.https.onRequest((req: any, res: any) => {
   const users: Array<any> = [];
   return admin.auth().listUsers().then((userRecords: any) => {
     userRecords.users.forEach((user: any) => (users.push(user)));
+    res.set('Access-Control-Allow-Origin', "*")
     res.end(JSON.stringify(users));
     console.log('Request successful!');
   }).catch((error: any) => console.log('Request failed: ', error));
@@ -117,6 +117,7 @@ exports.countAllUsers = functions.https.onRequest((req: any, res: any) => {
   return admin.auth().listUsers().then((userRecords: any) => {
     userRecords.users.forEach((user: any) => (users.push(user)));
     const count: String = `{ "count": ${users.length} }`;
+    res.set('Access-Control-Allow-Origin', "*")
     res.end(count);
     console.log('Request successful!');
   }).catch((error: any) => console.log('Request failed: ', error));
