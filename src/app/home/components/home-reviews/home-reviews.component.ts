a0acffc3d8a4de7c0298fc37b3a0a5515f7ea2ff
Richard Awoyemi <Richardawoyemi@gmail.com>
2019-07-18 17:32:36 +0100

    made some minor content changes

--- src/app/home/components/home-reviews/home-reviews.component.ts
+++ src/app/home/components/home-reviews/home-reviews.component.ts
@@ -6,17 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeReviewsComponent {
   reviews = [{
-    review: 'Brilliant service! We were matched with a talented developer who put together our new company '
+    review: 'Brilliant service! Their talented developer who put together our new company '
       + 'website within a couple of days.',
     author: 'Fred Obiosa, Co-Founder @ Ovents',
     image: '../assets/img/fred.jpg'
   }, {
-    review: 'There\'s nothing out there like it. I completely lost trust in freelancing platforms ' +
-      'until I found Opsonion.',
+    review: 'There\'s nothing out there like it. They engaged with our ideas and created, ' +
+      'something beautiful, really happy I found Opsonion.',
     author: 'Yinka Akomolafe, Co-Founder @ Marquess London',
     image: '../assets/img/jermaine.jpg'
   }, {
-    review: 'Our matched developer was a great fit and accomplished our objectives without any ' +
+    review: 'Our developer was a great fit and accomplished our objectives without any ' +
       'hassle. Looking forward to posting my next project.',
     author: 'Abigail Yemofio, Co-Founder @ AV Afrique',
     image: '../assets/img/abigail.jpg'
