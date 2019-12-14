a52fa5a88237b7e0e69a65e3027890047c7aafff
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2019-12-14 14:27:40 +0000

    reduce size of sidebar on 24 inch screens

--- src/app/modules/builder/builder.page.ts
+++ src/app/modules/builder/builder.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { BuilderService } from './builder.service';
 import { Subscription } from 'rxjs';
@@ -8,6 +8,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './builder.page.html'
 })
 export class BuilderComponent implements OnInit {
+  innerWidth: number;
   previewMode: boolean = false;
   sidebarClass: string = 'col-md-3';
   showcaseClass: string = 'col-md-9';
@@ -20,6 +21,9 @@ export class BuilderComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    console.log(window.innerWidth);
+
     this.ngxLoader.start();
     this.previewModeSubscription = this.builderService.previewMode.subscribe((response => {
       if (response) {
@@ -27,9 +31,24 @@ export class BuilderComponent implements OnInit {
         this.showcaseClass = 'col-md-12';
       } else {
         this.previewMode = false;
-        this.showcaseClass = 'col-md-9';
+        if (this.innerWidth > 1900) {
+          this.showcaseClass = 'col-md-10';
+          this.sidebarClass = 'col-md-2';
+        }
       }
     }));
     this.ngxLoader.stop();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth > 1900) {
+      this.showcaseClass = 'col-md-10';
+      this.sidebarClass = 'col-md-2';
+    } else {
+      this.showcaseClass = 'col-md-9';
+      this.sidebarClass = 'col-md-3';
+    }
   }
 }
